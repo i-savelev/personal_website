53b713f5a92727cc97aa783016961afe97b8f3f4
i-savelev <ilyasavelyev98@mail.ru>
2025-04-19 21:09:11 +0300

    add tags to html and css

--- personal_website/blog/views.py
+++ personal_website/blog/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Article
+from .models import Tag
 from django.core.paginator import Paginator
 
 def article_list(request):
     articles_list = Article.objects.filter(draft=True).order_by('-pub_date')
+    tags_list = Tag.objects.all()
     paginator = Paginator(articles_list, 3)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     pages_list = paginator.get_elided_page_range(number = page_obj.number, on_each_side=1, on_ends=1)
     context = {
+        'tags': tags_list,
         'articles_list': page_obj,
         'pages_list': pages_list,
         }
