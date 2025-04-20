8254ab77fed202ff0f42fd7d2faa7740cd968911
i-savelev <ilyasavelyev98@mail.ru>
2025-04-20 15:26:04 +0300

    add queries by tag

--- personal_website/blog/urls.py
+++ personal_website/blog/urls.py
@@ -7,4 +7,6 @@ app_name = "blog"
 urlpatterns = [
     path("", views.article_list, name="article_list"),
     path("blog/<int:article_id>", views.article, name="article"),
+    path("blog/allposts", views.article_all, name="article_all"),
+    path("blog/tag/<str:tag_name>", views.article_by_tag_list, name="article_by_tag_list"),
 ]
--- personal_website/blog/views.py
+++ personal_website/blog/views.py
@@ -7,7 +7,7 @@ from django.core.paginator import Paginator
 def article_list(request):
     articles_list = Article.objects.filter(draft=True).order_by('-pub_date')
     tags_list = Tag.objects.all()
-    paginator = Paginator(articles_list, 3)
+    paginator = Paginator(articles_list, 5)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     pages_list = paginator.get_elided_page_range(number = page_obj.number, on_each_side=1, on_ends=1)
@@ -17,7 +17,36 @@ def article_list(request):
         'pages_list': pages_list,
         }
     return render(request, 'blog/blog.html', context)
+
+def article_by_tag_list(request, tag_name):
+    articles_list = Article.objects.filter(draft=True, tags__name__icontains = tag_name).order_by('-pub_date')
+    tags_list = Tag.objects.all()
+    paginator = Paginator(articles_list, 5)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    pages_list = paginator.get_elided_page_range(number = page_obj.number, on_each_side=1, on_ends=1)
+
+    context = {
+        'tags': tags_list,
+        'articles_list': page_obj,
+        'pages_list': pages_list,
+        'tag_name': tag_name,
+        }
+    return render(request, 'blog/blog.html', context)
     
+
+def article_all(request):
+    articles_list = Article.objects.filter(draft=True).order_by('-pub_date')
+    paginator = Paginator(articles_list, 20)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    pages_list = paginator.get_elided_page_range(number = page_obj.number, on_each_side=1, on_ends=1)
+    context = {
+        'articles_list': page_obj,
+        'pages_list': pages_list,
+        }
+    print(f"Rendering template: blog/allposts.html")
+    return render(request, 'blog/blog_all_posts.html', context)    
 
 def article(request, article_id):
     article = get_object_or_404(Article, pk=article_id)
