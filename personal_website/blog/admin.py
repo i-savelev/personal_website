8b443fa2f927595725b0ddb1af7733f1135bacc5
i-savelev <ilyasavelyev98@mail.ru>
2025-04-20 16:18:34 +0300

    customize admin

--- personal_website/blog/admin.py
+++ personal_website/blog/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 
 from .models import Article, Tag
 
-admin.site.register(Article) # Регистрация моделей в админке
+class ArticleAdmin(admin.ModelAdmin):
+    fields = ['title', "pub_date","draft", "tags","description", "content"]
+    list_display = ('title','id', 'get_tags', 'pub_date', 'draft') 
+    filter_horizontal = ("tags",)
+    list_filter = ["draft"]
+    
+    def get_tags(self, obj):
+        return ", ".join([tag.name for tag in obj.tags.all()])
+    get_tags.short_description = 'Tags'
+
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Tag)
