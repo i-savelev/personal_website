9e681873bb617fe50ac2511604e17e70e39ae689
i-savelev <ilyasavelyev98@mail.ru>
2025-05-08 11:31:00 +0300

    fix

--- personal_website/mysite/urls.py
+++ personal_website/mysite/urls.py
@@ -21,8 +21,8 @@ from django.conf import settings
 
 
 urlpatterns = [
-    path("", include("blog.urls")),
     path('admin/', admin.site.urls),
+    path("", include("blog.urls")),
     path('markdownx/', include('markdownx.urls')),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
